5ab9e30a186a40439136a9590d9ecc55654faf07
ClementRoosDev <clement.roos@ynov.com>
2016-01-14 09:53:52 +0100

    Initial Commit of previous solution

---
+++ CompetitionJudo.BusinessDLL/Groupes/Sexe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompetitionJudo.Business
+{
+    public class Sexe
+    {
+    }
+
+    public enum Sexes
+    {
+        Tous,
+        M,
+        F
+    }
+}
